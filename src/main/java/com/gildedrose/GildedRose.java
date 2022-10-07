ea9a030c7df44e0e150a31adeb3ea33868130319
Brandon9627 <jb_yoan@ymail.com>
2022-10-07 14:18:29 +0200

    functions for items created

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,7 +2,6 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
-
     public GildedRose(Item[] items) {
         this.items = items;
     }
@@ -13,55 +12,75 @@ class GildedRose {
 
             switch (item.name) {
                 case "Aged Brie":
-                    item.sellIn--;
-                    if (item.quality < 50) {
-                        item.quality++;
-                    }
-                    if (item.sellIn < 0 && item.quality < 50) {
-                        item.quality++;
-                    }
+                    agedBrie(item);
                     break;
                 case "Backstage passes to a TAFKAL80ETC concert":
-                    item.sellIn--;
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    } else {
-                        if (item.quality < 50) {
-                            item.quality++;
-                        }
-                        if (item.sellIn < 10 && item.quality < 50) {
-                            item.quality++;
-                        }
-                        if (item.sellIn < 5 && item.quality < 50) {
-                            item.quality++;
-                        }
-                    }
-
+                    backstagePasses(item);
                     break;
                 case "Sulfuras, Hand of Ragnaros":
-                    item.quality = 80;
+                    sulfuras(item);
                     break;
                 case "Conjured":
-                    item.sellIn --;
-                    for(int i=0; i<2; i++){
-                        if (item.quality > 0) {
-                            item.quality--;
-                        }
-                        if (item.sellIn < 0 && item.quality > 0) {
-                            item.quality--;
-                        }
-                    }
+                    conjured(item);
                     break;
                 default:
-                    item.sellIn --;
-                    if (item.quality > 0) {
-                        item.quality --;
-                    }
-                    if (item.sellIn < 0 && item.quality > 0) {
-                        item.quality --;
-                    }
+                    anyItem(item);
                     break;
             }
         }
     }
+
+    private void agedBrie(Item item){
+        item.sellIn--;
+        if (item.quality < 50) {
+            item.quality++;
+        }
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void backstagePasses(Item item){
+        item.sellIn--;
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        } else {
+            if (item.quality < 50) {
+                item.quality++;
+            }
+            if (item.sellIn < 10 && item.quality < 50) {
+                item.quality++;
+            }
+            if (item.sellIn < 5 && item.quality < 50) {
+                item.quality++;
+            }
+        }
+    }
+
+    private void sulfuras(Item item){
+        item.quality = 80;
+    }
+
+    private void conjured(Item item){
+        item.sellIn --;
+        for(int i=0; i<2; i++){
+            if (item.quality > 0) {
+                item.quality--;
+            }
+            if (item.sellIn < 0 && item.quality > 0) {
+                item.quality--;
+            }
+        }
+    }
+
+    private void anyItem(Item item){
+        item.sellIn --;
+        if (item.quality > 0) {
+            item.quality --;
+        }
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality --;
+        }
+    }
+
 }
