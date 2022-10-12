35917fede53164cdea1ade53c195ca543c68dafd
Brandon9627 <jb_yoan@ymail.com>
2022-10-12 16:17:28 +0200

    Final

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,16 +5,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 class GildedRoseTest {
-// ********** Example given **********
-  @Test
-  @DisplayName("Test that the name is unchanged")
-  void testName() {
-    Item element = new Item("foo", 0, 0);
-    GildedRose app = new GildedRose(new Item[] {element});
-    app.updateQuality();
-    assertThat(element.name, is("foo"));
-  }
-
 // ********** Tests from requirements ***********
   @Test
   @DisplayName("Test that when SellIn<0, quality decreases by 2")
@@ -22,7 +12,7 @@ class GildedRoseTest {
       Item element = new Item("Any", 0, 2);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(0));
+      assertThat(element.toString(), is("Any, -1, 0"));
   }
 
   @Test
@@ -31,7 +21,7 @@ class GildedRoseTest {
       Item element = new Item("Any", 1, 0);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(0));
+      assertThat(element.toString(), is("Any, 0, 0"));
   }
 
   @Test
@@ -40,7 +30,7 @@ class GildedRoseTest {
       Item element = new Item("Aged Brie", 1, 40);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(41));
+      assertThat(element.toString(), is("Aged Brie, 0, 41"));
   }
 
   @Test
@@ -49,7 +39,7 @@ class GildedRoseTest {
       Item element = new Item("Aged Brie", 10, 50);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(50));
+      assertThat(element.toString(), is("Aged Brie, 9, 50"));
   }
 
   @Test
@@ -58,7 +48,7 @@ class GildedRoseTest {
       Item element = new Item("Sulfuras, Hand of Ragnaros", 10, 50);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(80));
+      assertThat(element.toString(), is("Sulfuras, Hand of Ragnaros, 9, 80"));
   }
 
   @Test
@@ -67,7 +57,7 @@ class GildedRoseTest {
       Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 20);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(21));
+      assertThat(element.toString(), is("Backstage passes to a TAFKAL80ETC concert, 11, 21"));
   }
 
   @Test
@@ -76,7 +66,7 @@ class GildedRoseTest {
       Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(22));
+      assertThat(element.toString(), is("Backstage passes to a TAFKAL80ETC concert, 10, 22"));
   }
 
   @Test
@@ -85,7 +75,7 @@ class GildedRoseTest {
       Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 20);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(23));
+      assertThat(element.toString(), is("Backstage passes to a TAFKAL80ETC concert, 5, 23"));
   }
   @Test
   @DisplayName("Backstage passes, will not raise if quality is 50 ")
@@ -93,7 +83,7 @@ class GildedRoseTest {
       Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 8, 50);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(50));
+      assertThat(element.toString(), is("Backstage passes to a TAFKAL80ETC concert, 7, 50"));
     }
   @Test
   @DisplayName("Backstage passes, will not raise above 50 if 5 <= sellin <= 10 and quality=49")
@@ -101,7 +91,7 @@ class GildedRoseTest {
         Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 8, 49);
         GildedRose app = new GildedRose(new Item[] {element});
         app.updateQuality();
-        assertThat(element.quality, is(50));
+        assertThat(element.toString(), is("Backstage passes to a TAFKAL80ETC concert, 7, 50"));
     }
 
   @Test
@@ -110,7 +100,7 @@ class GildedRoseTest {
         Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 48);
         GildedRose app = new GildedRose(new Item[] {element});
         app.updateQuality();
-        assertThat(element.quality, is(50));
+        assertThat(element.toString(), is("Backstage passes to a TAFKAL80ETC concert, 0, 50"));
     }
 
   @Test
@@ -119,7 +109,7 @@ class GildedRoseTest {
       Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(0));
+      assertThat(element.toString(), is("Backstage passes to a TAFKAL80ETC concert, -1, 0"));
   }
 
   @Test
@@ -128,7 +118,7 @@ class GildedRoseTest {
       Item element = new Item("Conjured", 1, 40);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(38));
+      assertThat(element.toString(), is("Conjured, 0, 38"));
   }
 
   @Test
@@ -137,7 +127,7 @@ class GildedRoseTest {
       Item element = new Item("Conjured", 0, 40);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(36));
+      assertThat(element.toString(), is("Conjured, -1, 36"));
   }
   @Test
   @DisplayName("Conjured will not decrease below zero when sellin < 0")
@@ -145,7 +135,7 @@ class GildedRoseTest {
         Item element = new Item("Conjured", 0, 2);
         GildedRose app = new GildedRose(new Item[] {element});
         app.updateQuality();
-        assertThat(element.quality, is(0));
+        assertThat(element.toString(), is("Conjured, -1, 0"));
     }
 
   @Test
@@ -154,7 +144,7 @@ class GildedRoseTest {
       Item element = new Item("Conjured", 10, 0);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(0));
+      assertThat(element.toString(), is("Conjured, 9, 0"));
     }
 
 // ********** Tests remaining to cover the entire code **********
@@ -165,7 +155,7 @@ class GildedRoseTest {
       Item element = new Item("Aged Brie", 0, 10);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(12));
+      assertThat(element.toString(), is("Aged Brie, -1, 12"));
   }
 
   @Test
@@ -174,7 +164,7 @@ class GildedRoseTest {
         Item element = new Item("Aged Brie", 0, 49);
         GildedRose app = new GildedRose(new Item[] {element});
         app.updateQuality();
-        assertThat(element.quality, is(50));
+        assertThat(element.toString(), is("Aged Brie, -1, 50"));
     }
 
   @Test
@@ -183,14 +173,7 @@ class GildedRoseTest {
         Item element = new Item("Sulfuras, Hand of Ragnaros", -1, 80);
         GildedRose app = new GildedRose(new Item[] {element});
         app.updateQuality();
-        assertThat(element.quality, is(80));
-    }
-
-  @Test
-  @DisplayName("Test Item toString return value")
-  void testStringReturn() {
-        Item element = new Item("Sulfuras, Hand of Ragnaros", 5, 10);
-        assertThat(element.toString(), is("Sulfuras, Hand of Ragnaros, 5, 10"));
+        assertThat(element.toString(), is("Sulfuras, Hand of Ragnaros, -2, 80"));
     }
 
 // ***** Test to complete Pitest *****
@@ -201,7 +184,7 @@ class GildedRoseTest {
       Item element = new Item("Any", 0, 1);
       GildedRose app = new GildedRose(new Item[] {element});
       app.updateQuality();
-      assertThat(element.quality, is(0));
+      assertThat(element.toString(), is("Any, -1, 0"));
   }
 
     @Test
@@ -210,6 +193,6 @@ class GildedRoseTest {
         Item element = new Item("Any", 1, 2);
         GildedRose app = new GildedRose(new Item[] {element});
         app.updateQuality();
-        assertThat(element.quality, is(1));
+        assertThat(element.toString(), is("Any, 0, 1"));
     }
 }
